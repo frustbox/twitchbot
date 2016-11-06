f1f23f8fd5a6b771cfd7bb5e3a719e02bab9c661
frustbox <frustbox@mailbox.org>
2016-11-06 19:05:30 +0100

    Refactor to make code reuse easier.
    Doesn't currently work because weechat can not use bound methods as a callback.
    
    The classes and objects should be somewhat functional.

--- twitchbot.py
+++ twitchbot.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-# -*- coding: utf-8 -*-
 import time
 import re
 from collections import namedtuple
@@ -10,6 +11,25 @@ except ImportError:
     print('This script must be run under WeeChat.')
     print('Get WeeChat now at: http://www.weechat.org/')
     import_ok = False
+
+
+SCRIPT_NAME = "weechat_twitchbot"
+SCRIPT_AUTHOR = "frustbox"
+SCRIPT_VERSION = "0.1"
+SCRIPT_LICENSE = "GPLv3"
+SCRIPT_DESCRIPTION = "This is an extensible bot for twitch chats."
+
+
+# =====================================
+# CONFIG
+# =====================================
+CHANNELS = {
+    'twitch': [
+        'frustbox',
+        'lorgon',
+        ],
+}
+OWNER = 'frustbox'
 
 DEBUG = False
 OK = weechat.WEECHAT_RC_OK  # tired of typing this all the time
@@ -92,364 +112,199 @@ class Timer(object):
 
 
 # =====================================
-# Timer stuff.
+# Bot objects
 # =====================================
-my_timer = Timer()
+class WeechatBot(object):
+    """Bot for weechat that can read messages and reply.
+
+    This is the IRC layer. Some other class can be dropped in here."""
+    network = None
+    channel = None
+    buffer = None
+    muted = False
+    owner = None
+
+    def __init__(self, network, channel):
+        """Connect to a network and channel"""
+        self.network = network
+        self.channel = channel
+        self.buffer = weechat.info_get('irc_buffer', '{},#{}'.format(network, channel))
+        self.owner = OWNER
+        weechat.hook_print(self.buffer, 'irc_privmsg', '', 1, self.callback, '')
+        super(self.__class__, self).__init__(network, channel)
+
+    def callback(self, sender, message):
+        """This is the callback method called by weechat."""
+        self.dispatch(sender, message)
+        return OK
+
+    def say(self, message=None, force=False):
+        """Speak, bot, speak!"""
+
+        if self.muted and not force:
+            return True
+
+        weechat.command(self.buffer, message)
+
+    def is_valid_nick(self, nick):
+        """Return true if `nick` is a valid irc nick."""
+
+    def nick_in_chat(self, nick):
+        return True
+
+    def is_op(self, nick):
+        """"""
+
+    def get_nicklist(self):
+        """Return the list of chatters."""
 
 
-def timer_start(buffer, nick):
-    """Start the timer."""
-    global my_timer
+class BotTwitchMixin(object):
+    def stream_is_live(self):
+        """Return whether or not the stream is live."""
 
-    if my_timer and not my_timer.running:
-        my_timer.start()
-        return bot_say(buffer, nick, u'Timer started.')
+    def get_stream_title(self):
+        """Return the title for the current stream."""
 
+    def get_viewers(self):
+        """Return the number of viewers."""
 
-def timer_restart(buffer, nick):
-    """Restart the timer."""
-    global my_timer
+    def is_subscriber(self, nick):
+        """Return True if `nick` is a subscriber to the channel."""
 
-    if my_timer and my_timer.running:
-        my_timer.restart()
-        return bot_say(buffer, nick, u'Timer restarted: {}'.format(my_timer.elapsed))
-
-
-def timer_add(buffer, nick, seconds):
-    """Add seconds to the timer."""
-    global my_timer
-
-    seconds = int(float(seconds))
-
-    if my_timer and my_timer.running:
-        my_timer.add(seconds)
-        return bot_say(buffer, nick, u'Added {} seconds to the timer: {}'.format(
-            seconds,
-            my_timer.elapsed,
-        ))
+    def get_uptime(self, nick):
+        """Return how long the stream has been going."""
 
 
-def timer_stop(buffer, nick):
-    """Stop the timer."""
-    global my_timer
+class BotTimerMixin(object):
+    """Bot functionality that implements timers."""
+    timer = Timer()
 
-    if my_timer and my_timer.running:
-        my_timer.stop()
-        return bot_say(buffer, nick, u'Timer stopped after {}'.format(my_timer.elapsed))
-
-
-def timer_split(buffer, nick, name):
-    """Add a split time to the timer."""
-    global my_timer
-
-    if my_timer and my_timer.running:
-        split = my_timer.split(name)
-        if split:
-            return bot_say(buffer, nick, u'Split "{}" created: {}'.format(split.name, split.time))
-
-    else:
-        return bot_say(buffer, nick, u'Timer must be running.')
+    def dispatch_op_commands(self, sender, message):
+        """"""
+    def dispatch_regular_commands(self, sender, message):
+        """"""
 
 
-def timer_status(buffer, nick):
-    """Print the status of the timer."""
-    global my_timer
+class BotCounterMixin(object):
+    """Keeps count how many times a command was used."""
+    counters = {}
 
-    if my_timer and my_timer.running:
-        return bot_say(buffer, nick, u'Timer running: {}'.format(my_timer.elapsed))
-    elif my_timer.stop_time:
-        return bot_say(buffer, nick, u'Timer stopped: {}'.format(my_timer.elapsed))
-    else:
-        return bot_say(buffer, nick, u'Timer not running')
+    def dispatch_everybody_commands(self, sender, message):
+        """"""
+
+    def dispatch_regular_commands(self, sender, message):
+        """"""
+
+    def dispatch_op_commands(self, sender, message):
+        """"""
 
 
-def timer_report(buffer, nick):
-    """Print a full report with all splits."""
-    global my_timer
+class BotFunMixin(object):
+    """Some fun commands."""
 
-    if my_timer:
-        splits = ", ".join(['{}: {}'.format(n, t) for n, t in my_timer.splits])
-        if my_timer.running and splits != '':
-            return bot_say(buffer, nick, u'Timer running: {}, with splits: {}'.format(
-                my_timer.elapsed,
-                splits
-            ))
-        elif my_timer.running:
-            return bot_say(buffer, nick, u'Timer running: {}, with no splits'.format(
-                my_timer.elapsed,
-            ))
-        elif my_timer.stop_time and splits:
-            return bot_say(buffer, nick, u'Timer stopped: {}, with splits: {}'.format(
-                my_timer.elapsed,
-                splits
-            ))
-        elif my_timer.stop_time:
-            return bot_say(buffer, nick, u'Timer stopped: {}, with no splits'.format(
-                my_timer.elapsed,
-            ))
+    def dispatch_regular_commands(self, sender, message):
+        """"""
+
+    def dispatch_everybody_commands(self, sender, message):
+        """"""
+
+    def command_info(self, sender):
+        return self.say(u'')
+
+    def command_luck(self, sender):
+        """Check if conedodger240 is in the viewer list and reply."""
+        if sender == 'conedodger240' or self.nick_in_chat('conedodger240'):
+            self.say(sender, u'Oh NO! ConeDodger240 is here. kurtCone')
         else:
-            return bot_say(buffer, nick, u'Whoops.')
+            self.say(sender, u'Rejoice! You\'re safe, the kurtCone has been dodged.')
 
 
-# =====================================
-# Just for Fun commands
-# =====================================
-def cone(buffer, nick):
-    """Check if conedodger240 is in the viewer list and reply."""
-    if DEBUG:
-        weechat.prnt("", 'DEBUG: luck')
-    if weechat.nicklist_search_nick(buffer, "", "conedodger240") or nick == 'conedodger240':
-        return bot_say(buffer, nick, u'Oh NO! ConeDodger240 is here. kurtCone')
-    else:
-        return bot_say(buffer, nick, u'Rejoice! You\'re safe, the kurtCone has been dodged.')
+class Bot(WeechatBot, BotTwitchMixin, BotTimerMixin, BotFunMixin):
+    """Bot functionality."""
 
-# =====================================
-# Bot control
-# =====================================
-OWNER = 'frustbox'
-USERS_OP = ['mateuszdrwal', 'lorgon']
-USERS_REGULAR = ['bobbyann', 'edi_j', 'marcmagus', 'mbxdllfs', 'docgratis', 'conedodger240']
-USERS_BLACKLIST = []
-WHITELISTED_NETWORKS = ['twitch']
-WHITELISTED_CHANNELS = ['#lorgon', '#frustbox']
-MUTED_BUFFERS = []
+    ops = []
+    regulars = []
+    blacklist = []
 
+    def __init__(self, network, channel):
+        super(self.__class__, self).__init__(network, channel)
 
-# OWNER commands
-def bot_op(buffer, nick, name):
-    """Add user to OP list."""
-    name = name.strip().lower()
-    if not weechat.info_get('irc_is_nick', name):
-        return bot_say(buffer, nick, u'That is not a valid username.')
+    def is_command(self, message):
+        return message.startswith(u'+')
 
-    if name == OWNER:
-        return bot_say(buffer, nick, u'{} is my owner.'.format(name))
+    def is_streamer(self, nick):
+        return nick == self.channel
 
-    if name in USERS_OP:
-        return bot_say(buffer, nick, u'{} is already OP'.format(name))
+    def is_owner(self, nick):
+        return nick == self.owner or self.is_streamer(nick)
 
-    if name in USERS_REGULAR:
-        USERS_REGULAR.remove(name)
+    def is_op(self, nick):
+        return nick in self.ops
 
-    USERS_OP.append(name)
-    return bot_say(buffer, nick, u'{} is now a regular.'.format(name))
+    def is_regular(self, nick):
+        return nick in self.regulars
 
+    def is_blacklisted(self, nick):
+        return nick in self.blacklist
 
-def bot_deop(buffer, nick, name):
-    """Remove user from OP list."""
-    name = name.strip().lower()
-    if not weechat.info_get('irc_is_nick', name):
-        return bot_say(buffer, nick, u'That is not a valid username.')
+    def can_use_regular(self, nick):
+        return self.is_owner(nick) or self.is_op(nick) or self.is_owner(nick)
 
-    if name == OWNER:
-        return bot_say(buffer, nick, u'{} is my owner.'.format(name))
+    def can_use_op(self, nick):
+        return self.is_owner(nick) or self.is_op(nick)
 
-    if name not in USERS_OP:
-        return bot_say(buffer, nick, u'{} is not an OP'.format(name))
+    def dispatch(self, sender, message):
+        """Make the bot do something."""
 
-    if name in USERS_OP:
-        USERS_OP.remove(name)
-        return bot_say(buffer, nick, u'{} is no longer an OP'.format(name))
+        if not self.is_command(message):
+            return
 
+        if self.is_blacklisted(sender):
+            return
 
-def bot_regular(buffer, nick, name):
-    """Add user to regulars list."""
-    global USERS_REGULAR
+        if self.is_owner(sender):
+            self.dispatch_owner_commands(sender, message)
 
-    name = name.strip().lower()
-    if not weechat.info_get('irc_is_nick', name):
-        return bot_say(buffer, nick, u'That is not a valid username.')
+        if self.can_use_op(sender):
+            self.dispatch_op_commands(sender, message)
 
-    if name == OWNER:
-        return bot_say(buffer, nick, u'{} is my owner.'.format(name))
+        if self.can_use_regular(sender):
+            self.dispatch_regular_commands(sender, message)
 
-    if name in USERS_OP:
-        return bot_say(buffer, nick, u'{} is already OP'.format(name))
+        self.dispatch_everybody_commands(sender, message)
 
-    if name in USERS_REGULAR:
-        return bot_say(buffer, nick, u'{} is already a regular'.format(name))
+    def dispatch_owner_commands(self, sender, message):
+        """Commands to be used by the owner."""
+        super(self.__class__, self).dispatch_owner_commands(sender, message)
 
-    USERS_REGULAR.append(name)
-    return bot_say(buffer, nick, u'{} is now a regular.'.format(name))
+    def dispatch_op_commands(self, sender, message):
+        """Commands to be used by ops."""
+        super(self.__class__, self).dispatch_op_commands(sender, message)
 
+    def dispatch_regular_commands(self, sender, message):
+        """Commands to be used by regulars."""
+        super(self.__class__, self).dispatch_regular_commands(sender, message)
 
-def bot_deregular(buffer, nick, name):
-    """Remove a user from regulars list."""
-    global USERS_REGULAR
-
-    name = name.strip().lower()
-    if not weechat.info_get('irc_is_nick', name):
-        return bot_say(buffer, nick, u'That is not a valid username')
-
-    if name not in USERS_REGULAR:
-        return bot_say(buffer, nick, u'{} is not a regular.'.forman(name))
-
-    USERS_REGULAR.remove(name)
-    return bot_say(buffer, nick, u'{} is no longer a regular.'.format(name))
-
-
-def bot_mute(buffer, nick):
-    """Add buffer to muted buffers list."""
-    global MUTED_BUFFERS
-    if buffer not in MUTED_BUFFERS:
-        MUTED_BUFFERS.append(buffer)
-        return bot_say(buffer, nick, u'I\'ll shut up.', True)
-
-
-def bot_unmute(buffer, nick):
-    """Remove buffer from muted buffers list."""
-    global MUTED_BUFFERS
-    if buffer in MUTED_BUFFERS:
-        MUTED_BUFFERS.remove(buffer)
-        return bot_say(buffer, nick, u'I can speak again.')
-
-
-# =====================================
-# Weechat stuff
-# =====================================
-def bot_say(buffer, nick, text, force=False):
-    """Make the bot say someting in `buffer`."""
-    if DEBUG:
-        weechat.prnt("", "DEBUG: bot_say -- {}".format(text))
-
-    if buffer in MUTED_BUFFERS and not force:
-        return OK
-
-    if nick == "frustbox":
-        time.sleep(0.25)
-
-    weechat.command(buffer, text)
-    return OK
-
-
-def bot_dispatch(buffer, nick, message):
-    """Make the bot do something."""
-    if DEBUG:
-        weechat.prnt("", "DEBUG: bot_dispatch -- {}".format(message))
-
-    # ===================
-    # Everybody commands:
-    # ===================
-    if message == '+lifefruit':
-        return bot_say(buffer, nick, u'Yum!')
-
-    # ===================
-    # Regulars commands:
-    # ===================
-    if not (nick in USERS_REGULAR or nick in USERS_OP or nick == OWNER):
-        if DEBUG:
-            weechat.prnt("", "not regular or higher")
-        return OK
-
-    # +timer split
-    split_match = re.match(r'^\+timer split (.*)', message)
-    if split_match:
-        return timer_split(buffer, nick, split_match.group(1))
-
-    # +timer add
-    add_match = re.match(r'^\+timer add (-?\d+)', message)
-    if add_match:
-        return timer_add(buffer, nick, add_match.group(1))
-    # +timer start
-    if message.startswith('+timer start'):
-        return timer_start(buffer, nick)
-    # +timer stop
-    if message == '+timer stop':
-        return timer_stop(buffer, nick)
-    # +timer restart
-    if message == '+timer restart':
-        return timer_restart(buffer, nick)
-    # +timer report
-    if message == '+timer report':
-        return timer_report(buffer, nick)
-
-    # +timer
-    if message.startswith('+timer'):
-        return timer_status(buffer, nick)
-
-    if message == '+luck':
-        return cone(buffer, nick)
-
-    # ===================
-    # OP commands
-    # ===================
-    if not (nick in USERS_OP or nick == OWNER):
-        if DEBUG:
-            weechat.prnt("", "not OP or higher")
-        return OK
-
-    match = re.match(r'^\+regular add (.*)', message)
-    if match:
-        return bot_regular(buffer, nick, match.group(1))
-    match = re.match(r'^\+regular remove (.*)', message)
-    if match:
-        return bot_deregular(buffer, nick, match.group(1))
-    if message == '+regulars':
-        return bot_say(buffer, nick, str(USERS_REGULAR))
-    if message.startswith('+mute'):
-        return bot_mute(buffer, nick)
-    if message.startswith('+unmute'):
-        return bot_unmute(buffer, nick)
-
-    # ===================
-    # OWNER commands:
-    # ===================
-    if nick != OWNER:
-        if DEBUG:
-            weechat.prnt("", "not owner")
-        return OK
-
-    if message.startswith('+op'):
-        return bot_op(buffer, nick, message)
-    if message.startswith('+deop'):
-        return bot_deop(buffer, nick, message)
-
-    # nothing happened.
-    return OK
-
-
-def bot_callback(data, buffer, date, tags, displayed, highlight, prefix, message):
-    """Forward messages from weechat to the bot."""
-    if DEBUG:
-        weechat.prnt("", "DEBUG: bot_callback -- {} -- {} -- {} -- {} -- {} -- {} -- {}".format(
-            buffer,
-            date,
-            tags,
-            displayed,
-            highlight,
-            prefix,
-            message,
-        ))
-    message = message.strip()
-
-    # ignore all messages that don't start with a "+"
-    if not message.startswith(u'+'):
-        return OK
-
-    # clean up the nick
-    nick = re.sub('[~@%]', '', prefix)
-
-    # ignore all messages not from whitelisted users.
-    if nick in USERS_BLACKLIST:
-        return OK
-
-    # actually do something.
-    return bot_dispatch(buffer, nick, message)
+    def dispatch_everybody_commands(self, sender, message):
+        """Commands that can be used by everybody. (Beware of spam!)"""
+        super(self.__class__, self).dispatch_everybody_commands(sender, message)
 
 
 if __name__ == '__main__' and import_ok:
-    weechat.register("weechat_timer",
-                     "frustbox",
-                     "0.1",
-                     "GPLv3",
-                     "This is a small timer module, allowing to create a timer, '\
-                     'start and stop it and even put in split times.",
-                     "",
-                     "")
+    weechat.register(
+        SCRIPT_NAME,
+        SCRIPT_AUTHOR,
+        SCRIPT_VERSION,
+        SCRIPT_LICENSE,
+        SCRIPT_DESCRIPTION,
+        "",
+        "")
+    weechat.prnt("", str(CHANNELS))
 
-    # have the bot listen in whitelisted networks and channels.
-    for network in WHITELISTED_NETWORKS:
-        for channel in WHITELISTED_CHANNELS:
-            b = weechat.info_get('irc_buffer', '{},{}'.format(network, channel))
-            weechat.hook_print(b, 'irc_privmsg', '', 1, 'bot_callback', '')
-    # weechat.hook_signal('twitch,irc_out_privmsg', 'bot_handler_outgoing', '')
+    #  have the bot listen in whitelisted networks and channels.
+    bots = {}
+    for network, channels in CHANNELS.items():
+        for channel in channels:
+            key = '{network}_{channel}'.format(network=network, channel=channel)
+            bots[key] = Bot(network=network, channel=channel)
